24e012eefcd401d27f5595f26324e5e7ed84dcc5
Luigi <luigi@umbrellavalidator.com>
2026-01-01 23:52:44 -0800

    Init with SwiftData for data storage

---
+++ WorkoutTrackerTests/WorkoutTrackerTests.swift
@@ -0,0 +1,17 @@
+//
+//  WorkoutTrackerTests.swift
+//  WorkoutTrackerTests
+//
+//  Created by luigi on 1/1/26.
+//
+
+import Testing
+@testable import WorkoutTracker
+
+struct WorkoutTrackerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
